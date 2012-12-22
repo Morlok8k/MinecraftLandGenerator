bda164b1b50c5f7099a1efeacf0e3f592e259db5
Morlok8k <otis.spankmeyer@gmail.com>
2012-12-22 00:03:01 -0800

    might be helpful if i updated the version number...

--- src/morlok8k/MinecraftLandGenerator/var.java
+++ src/morlok8k/MinecraftLandGenerator/var.java
@@ -28,7 +28,7 @@ public class var {
 	public static final String PROG_NAME = "Minecraft Land Generator";
 
 	/** Version Number! */
-	public static final String VERSION = "1.7.1";
+	public static final String VERSION = "1.7.2";
 
 	/** Authors */
 	public static final String AUTHORS = "Corrodias, Morlok8k, pr0f1x, jaseg";
